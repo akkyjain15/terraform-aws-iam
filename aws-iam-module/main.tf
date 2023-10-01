69005e5753771792c7adfb93bab842b382cb8fa1
Ubuntu <ubuntu@ip-172-31-27-52.us-west-2.compute.internal>
2023-10-01 19:21:31 +0000

    modifying files

--- aws-iam-module/main.tf
+++ aws-iam-module/main.tf
@@ -1,1 +1,41 @@
+data "aws_iam_policy_document" "example" {
+  statement {
+    actions   = ["s3:PutObject", "s3:GetObject", "s3:CreateBucket", "s3:DeleteObject", "s3:DeleteBucket"]
+    resources = ["*"]
+    effect = "Allow"
+  }
+#   statement {
+#     actions   = ["s3:*"]
+#     resources = [aws_s3_bucket.bucket.arn]
+#     effect = "Allow"
+#   }
+}
 
+resource "aws_iam_policy" "policy" {
+   name        = "s3-policy"
+   description = "My s3 test policy"
+   policy = data.aws_iam_policy_document.example.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "test-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
